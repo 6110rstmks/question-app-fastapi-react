b102ee63a490be58a228e5d6ff3155e103621eb9
sora-okinawa <165863455+sora-okinawa@users.noreply.github.com>
2024-10-28 10:06:53 +0900

    [WIP]まだバグがあるのでそれを修正。

--- frontend/src/components/ProblemPage.tsx
+++ frontend/src/components/ProblemPage.tsx
@@ -12,10 +12,14 @@ const ProblemPage: React.FC = () => {
     const location = useLocation();
     const problemData = location.state as Question[];
     const [currentProblemIndex, setCurrentProblemIndex] = useState(0);
-    const [currentReviewProblemIndex, setCurrentReviewProblemIndex] = useState(0);
-    const [unsolvedProblems, setUnsolvedProblems] = useState<Question[]>([]);
-    const [unsolvedProblemsIndex, setUnsolvedProblemsIndex] = useState(0);
     const [reviewFlg, setReviewFlg] = useState(false);
+
+    // 再出題用の変数
+    const [unsolvedProblems, setUnsolvedProblems] = useState<Question[]>([]);
+    const [unsolvedProblemsIndex, setUnsolvedProblemsIndex] = useState<number>(0);
+
+    // 再出題となった問題の数を表す変数
+    const [currentReviewProblemIndex, setCurrentReviewProblemIndex] = useState(0);
 
     const handleAnswer_solved = () => {
         setCurrentProblemIndex(currentProblemIndex + 1);
@@ -28,24 +32,43 @@ const ProblemPage: React.FC = () => {
 
     // 再度出題した用の関数
     const handleAnswer_solved_review = () => {
+        console.log(unsolvedProblems[currentReviewProblemIndex].id)
+        setUnsolvedProblems(unsolvedProblems.filter((problem) => problem.id !== unsolvedProblems[currentReviewProblemIndex].id));
+        console.log(unsolvedProblems)
         setCurrentReviewProblemIndex(currentReviewProblemIndex + 1);
     };
 
     // 再度出題した用の関数
     const handleAnswer_unsolved_review = () => {
-        setUnsolvedProblems(unsolvedProblems.filter((problem) => problem.id !== unsolvedProblems[currentReviewProblemIndex].id));
-        setCurrentReviewProblemIndex(currentReviewProblemIndex + 1);
+        setUnsolvedProblemsIndex(unsolvedProblemsIndex + 1);
     };
 
     // もう一度解けなかった問題に絞って解く。
     const handleReview = () => {
+        console.log(unsolvedProblems)
         setReviewFlg(true)
         setCurrentProblemIndex(0);
+        setCurrentReviewProblemIndex(0);
     }
 
-    if (currentProblemIndex >= problemData.length) {
+    if (currentProblemIndex == problemData.length) {
         return <div>
             <h1>終了</h1>
+            {unsolvedProblems.length > 0 ? (
+                <button onClick={handleReview}>解けなかった問題に絞ってもう一度復習する</button>
+
+            ) : (
+                <div>
+                    <div>全問正解</div>
+                    <button>ホームに戻る</button>
+                </div>
+            )}
+        </div>;
+    }
+
+    if (reviewFlg && currentReviewProblemIndex > unsolvedProblems.length) {
+        return <div>
+            <h1>【再出題】終了</h1>
             <button onClick={handleReview}>解けなかった問題に絞ってもう一度復習する</button>
         </div>;
     }
@@ -64,7 +87,7 @@ const ProblemPage: React.FC = () => {
                 </div>
             ) : (
                 <div>
-                    <h1>問題{currentProblemIndex + 1}/{unsolvedProblems.length}</h1>
+                    <h1>【再出】問題{currentReviewProblemIndex + 1}/{unsolvedProblems.length}</h1>
                     <div>
                         <h2>{unsolvedProblems[unsolvedProblemsIndex].problem}</h2>
                         <h2>{unsolvedProblems[unsolvedProblemsIndex].answer}</h2>
