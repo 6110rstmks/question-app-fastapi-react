ed92e129ee77ea3b0e6987e1c56dbfd49b8c784b
sora-okinawa <165863455+sora-okinawa@users.noreply.github.com>
2024-12-17 00:01:51 +0900

    edit: change min length of character of 'memo'

--- backend/schemas/question.py
+++ backend/schemas/question.py
@@ -8,14 +8,15 @@ from datetime import datetime
 class QuestionCreate(BaseModel):
     problem: str = Field(min_length=2, max_length=99, examples=["列志向データベースの強みを説明せよ"])
     answer: List[str] = Field(..., example=["Answer1", "Answer2"])
-    memo: str = Field(min_length=2, max_length=99, examples=["ここにquestionに関するメモを記入できます。"])
+    memo: str = Field(max_length=99, examples=["ここにquestionに関するメモを記入できます。"])
     category_id: int = Field(gt=0, example=1)
     subcategory_id: int = Field(gt=0, example=1)
     
 class QuestionUpdate(BaseModel):
     problem: str = Field(min_length=2, max_length=99, examples=["列志向データベースの強みを説明せよ"])
     answer: List[str] = Field(..., example=["Answer1", "Answer2"])
-    memo: str = Field(min_length=2, max_length=99, examples=["ここにquestionに関するメモを記入できます。"])
+    memo: str = Field(max_length=99, examples=["ここにquestionに関するメモを記入できます。"])
+    # memo: str = Field(min_length=2, max_length=99, examples=["ここにquestionに関するメモを記入できます。"])
     is_correct: bool = Field(default=False, example=False)
     
 class QuestionIsCorrectUpdate(BaseModel):
@@ -25,7 +26,7 @@ class QuestionResponse(BaseModel):
     id: int = Field(gt=0, examples=[1])
     problem: str = Field(min_length=2, max_length=99, examples=["列志向データベースの強みを説明せよ"], description="問題文")
     answer: List[str] = Field(..., example=["Answer1", "Answer2"])
-    memo: str = Field(min_length=2, max_length=99, examples=["ここにquestionに関するメモを記入できます。"])
+    memo: str = Field(max_length=99, examples=["ここにquestionに関するメモを記入できます。"])
     is_correct: bool = Field(default=False, example=False)
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
