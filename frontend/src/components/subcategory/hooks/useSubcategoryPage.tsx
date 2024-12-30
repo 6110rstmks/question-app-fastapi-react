2f7101a92728d9e67f68eeabed1f574e58adf2a9
sora-okinawa <165863455+sora-okinawa@users.noreply.github.com>
2024-12-31 03:29:42 +0900

    edit

--- frontend/src/components/subcategory/hooks/useSubcategoryPage.tsx
+++ frontend/src/components/subcategory/hooks/useSubcategoryPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useEffect, useState } from 'react'
 import { fetchSubcategory } from '../../../api/SubcategoryAPI'
 import { fetchQuestionsBySubcategoryId } from '../../../api/QuestionAPI'
