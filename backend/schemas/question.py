3399b03d8f44710f7d1a814d2963a3bd50404522
sora-okinawa <165863455+sora-okinawa@users.noreply.github.com>
2025-08-28 12:36:46 +0900

    edit

--- backend/schemas/question.py
+++ backend/schemas/question.py
@@ -4,14 +4,14 @@ from datetime import datetime
 from backend.config import SolutionStatus
 
 class QuestionCreate(BaseModel):
-    problem: str = Field(min_length=2, max_length=999, examples=["列志向データベースの強みを説明せよ"])
+    problem: str = Field(min_length=2, max_length=9999, examples=["列志向データベースの強みを説明せよ"])
     answer: List[str] = Field(..., example=["Answer1", "Answer2"])
     memo: str = Field(max_length=300, examples=["ここにquestionに関するメモを記入できます。"])
     category_id: int = Field(gt=0, example=1)
     subcategory_id: int = Field(gt=0, example=1)
     
 class QuestionUpdate(BaseModel):
-    problem: str = Field(min_length=2, max_length=999, examples=["列志向データベースの強みを説明せよ"])
+    problem: str = Field(min_length=2, max_length=9999, examples=["列志向データベースの強みを説明せよ"])
     answer: List[str] = Field(..., example=["Answer1", "Answer2"])
     memo: str = Field(max_length=999, examples=["ここにquestionに関するメモを記入できます。"])
     is_correct: SolutionStatus = Field(default=SolutionStatus.Incorrect, example=SolutionStatus.Incorrect)
@@ -26,7 +26,7 @@ class QuestionBelongsToSubcategoryIdUpdate(BaseModel):
    
 class QuestionResponse(BaseModel):
     id: int = Field(gt=0, examples=[1])
-    problem: str = Field(min_length=1, max_length=999, examples=["列志向データベースの強みを説明せよ"], description="問題文")
+    problem: str = Field(min_length=1, max_length=9999, examples=["列志向データベースの強みを説明せよ"], description="問題文")
     answer: List[str] = Field(..., example=["Answer1", "Answer2"])
     memo: str = Field(max_length=999, examples=["ここにquestionに関するメモを記入できます。"])
     is_correct: SolutionStatus = Field(default=SolutionStatus.Incorrect, example="NOT_SOLVED")
