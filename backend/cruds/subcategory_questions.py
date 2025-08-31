ea73b2aa551499172b17652970c3c290db9629f6
sora-okinawa <165863455+sora-okinawa@users.noreply.github.com>
2025-09-01 02:56:08 +0900

    edit

--- backend/cruds/subcategory_questions.py
+++ backend/cruds/subcategory_questions.py
@@ -1,4 +1,15 @@
 from backend.models import SubcategoryQuestion
+from sqlalchemy.orm import Session
+from backend.schemas.subcategory_question import SubcategoryQuestionResponse
+from typing import Optional
+from sqlalchemy import select
 
-def find_subcategory_question_by_question_id(db, question_id: int):
-    return db.query(SubcategoryQuestion).where(SubcategoryQuestion.question_id == question_id).first()    
+def find_subcategory_question_by_question_id(
+    db: Session, 
+    question_id: int
+) -> Optional[SubcategoryQuestionResponse]:
+    query = (
+        select(SubcategoryQuestion).where(SubcategoryQuestion.question_id == question_id)
+    )
+
+    return db.execute(query).scalars().first()
